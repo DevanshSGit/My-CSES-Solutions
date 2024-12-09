55658d29d1fb3b8987628a0f4cf95775e34a56c3
Devansh Sharma <devanshsh25@gmail.com>
2024-12-09 23:54:04 +0530

    TwoKnights

---
+++ TwoKnights.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+int main()
+{
+   ios::sync_with_stdio();
+   cin.tie(0);
+
+   ll n;
+   cin >> n;
+
+   // Choosing two cells from k^2 cells
+   // k^2 C 2 = (k^2 * (k^2 - 1))/2
+
+   return 0;
+}
