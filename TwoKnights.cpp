a28c21a424d93483a8f4468da56e6e51e55ed98f
Devansh Sharma <devanshsh25@gmail.com>
2024-12-10 00:10:30 +0530

    TwoKnights

--- TwoKnights.cpp
+++ TwoKnights.cpp
@@ -11,11 +11,20 @@ int main()
    ll n;
    cin >> n;
 
+
+   for (ll k = 1; k <= n; ++k) {
    // Choosing two cells from k^2 cells
    // k^2 C 2 = (k^2 * (k^2 - 1))/2
-
+    ll totalWays = (1LL*k*k) * (1LL*k*k-1)/2;
    // Number of attacking positions for two knights
    // 4*(k-1)*(k-2)
+    ll attackingWays = 0;
+    if (k > 2) {
+        attackingWays = 4*(k-1)*(k-2);
+    }
+    ll nonAttackingWays = totalWays - attackingWays;
+    cout << nonAttackingWays << '\n';
+   }
 
    return 0;
 }
