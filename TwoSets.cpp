8c6e2acc996d3fc30d7eec5fe2c0eb3120ac496f
Devansh Sharma <devanshsh25@gmail.com>
2024-12-23 22:58:36 +0530

    TwoSets

---
+++ TwoSets.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+int main()
+{
+   ios::sync_with_stdio();
+   cin.tie(0);
+
+   ll n;
+   cin >> n;
+
+   cout << n*(n+1)/2 << endl;
+
+   return 0;
+}
