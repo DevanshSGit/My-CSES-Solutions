a3f35f7c26ab93c7bb444a2cf6e049456e4de2d5
Devansh Sharma <devanshsh25@gmail.com>
2024-12-26 22:49:34 +0530

    TwoSets

--- TwoSets.cpp
+++ TwoSets.cpp
@@ -19,5 +19,11 @@ int main()
    // for n mod 4 = 0
    // (i, i+1, i+2, i+3), sum: 4i + 6
 
+   ll sum = n*(n+1)/2;
+   if (sum % 2 != 0) {
+    cout << "NO\n";
+    return 0;
+   }
+
    return 0;
 }
