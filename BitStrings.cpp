0d689762d0dbb144b3af07dc11dd032ebd3082b4
Devansh Sharma <devanshsh25@gmail.com>
2024-12-30 23:26:54 +0530

    BitStrings

--- BitStrings.cpp
+++ BitStrings.cpp
@@ -1,1 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+int main()
+{
+   ios::sync_with_stdio();
+   cin.tie(0);
+
+   ll n;
+   cin >> n;
+
+   // n = 3
+   // 000, 001, 010, 011, 100, 101, 110, 111
+
+
+   return 0;
+}
 
